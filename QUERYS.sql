575d6935073b865d8fb892041aa91b37dd75e2f9
Aleex555 <95920070+Aleex555@users.noreply.github.com>
2023-01-20 19:01:29 +0100

    Add files via upload

--- QUERYS.sql
+++ QUERYS.sql
@@ -3,6 +3,9 @@
  /*9*/select id_game,round(avg(bet),2) from ROUNDS  where round_number=1
 group by id_game;
 
+/*10*/select id_game,round(avg(bet),2) from ROUNDS  where round_number=(select number_rounds from GAME where round_number=number_rounds)
+group by id_game;
+
  /*2*/select id_game,any_value(id_player),min(bet) from ROUNDS group by id_game;
  /*3*/select id_game,any_value(id_player),max(bet) from ROUNDS group by id_game;
 
